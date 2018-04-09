1710f78cd27cb121d087b9345b930296d63fa8e6
Martin Bing <bing.martin.photography@gmail.com>
2018-04-09 20:49:44 +0200

    Add labels to itemcell and test them

--- Second Demo/ToDo/Tests/ItemCellTests.swift
+++ Second Demo/ToDo/Tests/ItemCellTests.swift
@@ -10,18 +10,64 @@ import XCTest
 @testable import ToDo
 
 class ItemCellTests: XCTestCase {
+    var tableView: UITableView!
+    let dataSource = FakeDataSource()
+    var cell: ItemCell!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard
+            .instantiateViewController(
+                withIdentifier:"ItemListViewController")
+            as! ItemListViewController
+        controller.loadViewIfNeeded()
+        tableView = controller.tableView
+        tableView?.dataSource = dataSource
+        cell = tableView?.dequeueReusableCell(
+            withIdentifier: "ItemCell",
+            for: IndexPath(row: 0, section: 0)) as! ItemCell
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
+
+    func test_HasTitleLabel() {
+        XCTAssertTrue(cell.titleLabel.isDescendant(of: cell.contentView))
+    }
     
+    func test_HasLocationLabel() {
+        XCTAssertTrue(cell.locationLabel.isDescendant(of: cell.contentView))
+    }
     
+    func test_HasDateLabel() {
+        XCTAssertTrue(cell.dateLabel.isDescendant(of: cell.contentView))
+    }
+
+    func test_ConfigCell_SetsTitle() {
+        cell.configCell(with: ToDoItem(title: "Foo"))
+
+        XCTAssertEqual(cell.titleLabel.text, "Foo")
+    }
+    
+    func test_ConfigCell_SetsLocation() {
+        cell.configCell(with: ToDoItem(title: "Foo", location: Location(name: "Bar")))
+        
+        XCTAssertEqual(cell.locationLabel.text, "Bar")
+    }
+    
+    func test_ConfigCell_SetsDate() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        let date = dateFormatter.date(from: "08/27/2017")
+        let timestamp = date?.timeIntervalSince1970
+        cell.configCell(with: ToDoItem(title: "Foo",
+                                       timestamp: timestamp))
+
+        XCTAssertEqual(cell.dateLabel.text, "08/27/2017")
+    }
 }
 
 extension ItemCellTests {
--- Second Demo/ToDo/ToDo/Controller/ItemCell.swift
+++ Second Demo/ToDo/ToDo/Controller/ItemCell.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ItemCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }()
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,8 +29,14 @@ class ItemCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
-    func configCell(with item: ToDoItem) {
-    }
 
+    func configCell(with item: ToDoItem) {
+        titleLabel.text = item.title
+        locationLabel.text = item.location?.name
+
+        if let timestamp = item.timestamp {
+            let date = Date(timeIntervalSince1970: timestamp)
+            dateLabel.text = dateFormatter.string(from: date)
+        }
+    }
 }
